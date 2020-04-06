c7be946aff65e23e6355d3faed0e44bd1becba40
Dominik Antunovic <dominik.antunovic@gmail.com>
2020-04-06 15:15:42 +0200

    added first functional navigation elements and shopping list features

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+import { Ingredient } from "src/app/shared/ingredient.model";
 
 @Component({
-  selector: 'app-shopping-edit',
-  templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  selector: "app-shopping-edit",
+  templateUrl: "./shopping-edit.component.html",
+  styleUrls: ["./shopping-edit.component.css"]
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onAdd(nameInput: string, amountInput: number) {
+    const newIngredient = new Ingredient(nameInput, amountInput);
+    this.ingredientAdded.emit(newIngredient);
   }
-
 }
