79632729e8609de92106c9c2b253d294b0ddc8b4
almightyblob <dominik.antunovic@gmail.com>
2020-05-11 14:26:23 +0200

    added services to recipes and ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,23 +1,35 @@
 import { Recipe } from "./recipe.model";
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
+  constructor(private shoppingListService: ShoppingListService) {}
+
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
     new Recipe(
-      "Test Recipe",
-      "just a test",
-      "https://rasamalaysia.com/wp-content/uploads/2018/04/beef-rendang.jpg"
+      "Rendang",
+      "Awesome Stuff",
+      "https://rasamalaysia.com/wp-content/uploads/2018/04/beef-rendang.jpg",
+      [new Ingredient("Meat", 1), new Ingredient("Rendang Sauce", 1)]
     ),
+
     new Recipe(
-      "Test Recipe 2",
-      "just a test",
-      "https://rasamalaysia.com/wp-content/uploads/2018/04/beef-rendang.jpg"
+      "Babi Ketjap",
+      "Npm nom",
+      "https://www.dewereldkeukenthuis.com/wp-content/uploads/2015/06/Babi-ketjap-600x900.jpg",
+      [new Ingredient("Babi", 1), new Ingredient("Ketjap", 1)]
     ),
   ];
 
   getRecipes() {
     return [...this.recipes];
+  }
+
+  addIngredientToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
