486a17915f2543a3a4a4cb553ad7193912cb56b3
almightyblob <dominik.antunovic@gmail.com>
2020-05-28 17:07:56 +0200

    working on adding working forms. Shopping list completed, recipes WIP

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -4,6 +4,7 @@ import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   ingredients: Ingredient[] = [
     new Ingredient("Apples", 5),
@@ -12,6 +13,20 @@ export class ShoppingListService {
 
   getIngredients() {
     return [...this.ingredients];
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index]
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
+    this.ingredientsChanged.next([...this.ingredients])
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next([...this.ingredients])
   }
 
   addIngredient(ingredient) {
